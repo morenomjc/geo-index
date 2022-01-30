7aed69978672ad581fd0ef335f6b2211c68bb78d
morenomjc <moreno.mark.john@gmail.com>
2022-01-30 19:17:16 +0800

    disable ddl-auto generation

--- src/main/java/com/morenomjc/geoindex/service/postgis/GeometricEntity.java
+++ src/main/java/com/morenomjc/geoindex/service/postgis/GeometricEntity.java
@@ -26,7 +26,7 @@ import javax.persistence.Table;
 public class GeometricEntity {
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
 	@NaturalId
